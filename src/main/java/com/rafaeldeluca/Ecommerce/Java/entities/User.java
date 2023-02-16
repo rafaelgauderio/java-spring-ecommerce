59fccc04469ae44f542a70e0968a40bfeea14863
Rafael de Luca <deluca1712@gmail.com>
2023-02-16 13:41:46 -0300

    Criando entidades User e arquivo application-properties

---
+++ src/main/java/com/rafaeldeluca/Ecommerce/Java/entities/User.java
@@ -0,0 +1,118 @@
+package com.rafaeldeluca.Ecommerce.Java.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+//mapeamentos da jpa
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String CPF_CNPJ;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private String senha;
+
+    public User () {
+
+    }
+
+    public User(Long id, String nome, String email, String CPF_CNPJ, String telefone, LocalDate dataNascimento, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.CPF_CNPJ = CPF_CNPJ;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCPF_CNPJ() {
+        return CPF_CNPJ;
+    }
+
+    public void setCPF_CNPJ(String CPF_CNPJ) {
+        this.CPF_CNPJ = CPF_CNPJ;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User {" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", CPF_CNPJ='" + CPF_CNPJ + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+}
