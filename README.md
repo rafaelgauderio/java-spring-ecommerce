d2e0f96bc9b3721a31252471f9bc3c4671c8e2be
Rafael de Luca <deluca1712@gmail.com>
2023-02-16 12:04:06 -0300

    Criando projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+## ecommerce com java, spring web, spring jpa, h2 database 
---
+++ src/test/java/com/rafaeldeluca/Ecommerce/Java/EcommerceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.Ecommerce.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
